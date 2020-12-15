1beb215181a280f4898b7bd6d1bd95b60d9ec64c
kunxin-chor <chorkunxin@yahoo.com>
2020-12-15 14:06:59 +0000

    Can pass key events to props

--- keyboard-test/src/App.js
+++ keyboard-test/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import BoxComponent from "./BoxComponent";
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   componentDidMount() {
@@ -9,14 +9,41 @@ class App extends React.Component {
     document.getElementById("App").focus();
   }
 
-  handleKey = () => {
-    alert('hi');
-  }
+  handleKey = e => {
+      console.log(e.keyCode)
+    if (e.keyCode === 65) {
+      this.setState({
+        a_pressed: true
+      });
+      setTimeout(() => {
+          console.log("reset");
+        this.setState({
+          a_pressed: false
+        });
+      }, 1000);
+    }
+    if (e.keyCode === 66) {
+      this.setState({
+        b_pressed: true
+      });
+      setTimeout(() => {
+        this.setState({
+          b_pressed: false
+        });
+      }, 1000);
+    }
+  };
+
+  state = {
+    a_pressed: false,
+    b_pressed: false
+  };
 
   render() {
     return (
       <div className="App" id="App" onKeyDown={this.handleKey} tabIndex={-1}>
-        <BoxComponent />
+        <BoxComponent keyIsDown={this.state.a_pressed} name="A" />
+        <BoxComponent keyIsDown={this.state.b_pressed} name="B"/>
       </div>
     );
   }
--- keyboard-test/src/BoxComponent.js
+++ keyboard-test/src/BoxComponent.js
@@ -1,6 +1,12 @@
 import React from "react"
 
 export default class BoxComponent extends React.Component {
+    componentDidUpdate(prevProps) {
+        if (this.props.keyIsDown){
+            console.log("I detect my key is down -- " + this.props.name)
+        }
+    }
+    
     render() {
         return (<div>
             Hello
